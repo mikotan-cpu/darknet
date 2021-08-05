1b6275596859d93ba6e4cbd3bcb4330f612162d9
mikotan-cpu <68640929+mikotan-cpu@users.noreply.github.com>
2021-08-05 19:48:31 +0800

    Update darknet.py

--- build/darknet/x64/darknet.py
+++ build/darknet/x64/darknet.py
@@ -134,7 +134,7 @@ def draw_boxes(detections, image, colors):
     for label, confidence, bbox in detections:
         left, top, right, bottom = bbox2points(bbox)
         cv2.rectangle(image, (left, top), (right, bottom), colors[label], 1)
-        cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
+        cv2.putText(image, "{} [{:.2f}]".format("HATDOG", float(confidence)),
                     (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     colors[label], 2)
     return image
